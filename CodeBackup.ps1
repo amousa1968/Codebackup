08eb16862ed50803cb6737452eb1658f99e4198e
amousa1968 <amousa@cox.net>
2017-09-14 17:52:26 -0700

    branch_files_branch1

---
+++ CodeBackup.ps1
@@ -0,0 +1,42 @@
+﻿##Backupcode.ps1  
+#MAINTAINER ayman.mousa <ayman.mousa@smtp.com>
+#simple script to backup code on .net servers
+
+$today= get-date -Format "MMddyyyy"
+
+
+$backup_path= "C:\backup\$today"
+
+$path=Test-Path $backup_path -PathType Container
+if (!$path)
+{
+
+    New-Item $backup_path -type directory
+}
+else
+{
+    $i=0
+    while ($path)
+    {
+    $backup_path_new=$backup_path+"_"+$i
+    Write-Host $backup_path_new
+    $path=Test-Path $backup_path_new -PathType Container
+
+    if (!$path)
+        {
+        New-Item $backup_path_new -type directory
+        }
+    else
+        {
+        $i++
+        }
+
+
+    }
+$backup_path=$backup_path_new
+}
+
+
+
+Copy-Item C:\inetpub\wwwroot\ $backup_path -Recurse
+
---
+++ CodeSync.ps1
@@ -0,0 +1,31 @@
+﻿# This file created and MAINTAINER ayman.mousa <ayman.mousa@smtp.com>
+#copyCode.ps1
+
+#simple script to sync code to a list of servers
+
+
+$serverlist = Get-Content(".\serverlist.txt")
+
+foreach ($server in $serverlist)
+    {
+     write-host "Coping to $server"   
+     $remotepath= "\\"+$server+"\c$\inetpub\wwwroot"
+     $copyPath = "C:\inetpub\wwwroot\"
+     Start-job -ScriptBlock{param ($copyPath,$remotepath) Copy-Item $copyPath $remotepath -Recurse -Force} -Argumentlist $copyPath,$remotepath
+     
+    }
+$i=0
+$runningjobs=@(get-job -state running).count 
+
+while ($runningjobs -ne 0)
+    {
+    Start-Sleep -Seconds 2
+    $runningjobs=@(get-job -state running).count    
+    $i++
+    $i++
+    Write-Host "Code Sync has been running for $i seconds.  Please be patient"
+    }
+ 
+  
+
+    Write-Host "Code Sync Complete" 
